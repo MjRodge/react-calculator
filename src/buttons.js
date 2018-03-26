7f9085addbfa0b99b6d3c5ca4657885eaf5ee1cb
Michael Rodgers <mail@mjrodge.me>
2018-03-26 12:14:35 -0400

    Screen and Buttons Components
    Added components for screen section and buttons sections

---
+++ src/buttons.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './css/buttons.css';
+
+class Buttons extends Component {
+  render() {
+    return (
+      <div id="calc-button-container">
+        <div id="calc-number-buttons">
+          // Components for number buttons here
+        </div>
+        <div id="calc-operand-buttons">
+          // Components for operand buttons here
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Buttons;
---
+++ src/screen.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './css/screen.css';
+
+class Screen extends Component {
+  render() {
+    return (
+      <div id="calc-screen">
+        // Components for calc-result and calc-full-input here
+      </div>
+    );
+  }
+}
+
+export default Screen;
