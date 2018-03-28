5e33adef918b37c67625825e52302b2c01cd0702
Michael Rodgers <mail@mjrodge.me>
2018-03-28 16:00:28 -0400

    Props Handling
    Added state to App component, to allow button click to be passed into
    Screen component

--- src/App.js
+++ src/App.js
@@ -6,14 +6,21 @@ import './css/buttons.css';
 import './css/App.css';
 
 class App extends Component {
-  buttonInput(test) {
-    alert(test);
+  constructor(props) {
+    super(props);
+    this.state = {
+      result: 0
+    };
+  }
+
+  buttonInput = (test) => { //ES6 function declaration to avoid having to bind
+    this.setState({ result: test });
   }
 
   render() {
     return (
       <div id="calc-container">
-        <Screen />
+        <Screen passedResult={this.state.result}/>
         <div id="calc-button-container">
           <NumberButtons keyPressed={this.buttonInput} />
           <OperandButtons keyPressed={this.buttonInput} />
--- src/screen.js
+++ src/screen.js
@@ -2,20 +2,11 @@ import React, { Component } from 'react';
 import './css/screen.css';
 
 class Screen extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-        result: 0,
-        fullInput: 0
-    };
-    //handleClick = handleClick.bind(this);
-  }
-
   render() {
     return (
       <div id="calc-screen">
-        <p id="calc-result">{this.state.result}</p>
-        <p id="calc-full-input">{this.state.fullInput}</p>
+        <p id="calc-result">{this.props.passedResult}</p>
+        <p id="calc-full-input">0</p>
       </div>
     );
   }
